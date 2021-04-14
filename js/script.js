dea38aabde0ec0e431a34aa0a06b2958a0ab4dca
Karaoke <karaoke@karaokes-mini.home>
2021-04-14 10:50:05 +0100

    Loads

--- js/script.js
+++ js/script.js
@@ -1,36 +1,56 @@
-// let burger = document.getElementById('burger'),
-// 	 nav    = document.getElementById('main-nav'),
-// 	 slowmo = document.getElementById('slowmo');
-
-// burger.addEventListener('click', function(e){
-// 	this.classList.toggle('is-open');
-// 	nav.classList.toggle('is-open');
-// });
-
-// slowmo.addEventListener('click', function(e){
-// 	this.classList.toggle('is-slowmo');
-// });
-
-// /* Onload demo - dirty timeout */
-// let clickEvent = new Event('click');
-
-// window.addEventListener('load', function(e) {
-// 	slowmo.dispatchEvent(clickEvent);
-// 	burger.dispatchEvent(clickEvent);
-	
-// 	setTimeout(function(){
-// 		burger.dispatchEvent(clickEvent);
-		
-// 		setTimeout(function(){
-// 			slowmo.dispatchEvent(clickEvent);
-// 		}, 3500);
-// 	}, 5500);
-// });
-
   $(function(){
-  $(".footer").load("/footer.html")
+  $(".footer").load("/components/footer.html")
   });
 
   $(function(){
-    $(".desktop__nav").load("/nav.html")
+    $(".desktop__nav").load("/components/nav.html")
     });
+
+  $(function() {
+    $('.mobile__nav').load("/components/mobileNav.html")
+  })
+
+  $(function() {
+    $('#sideNav').load("/components/sideNav.html")
+  })
+  
+  $(function() {
+    $('.contact__container').load("/components/contact.html")
+  })
+
+// Sidenav Variables
+
+let menuStatus = closed;
+
+// Open SideNav
+
+function openNav() {
+
+  $('#sideNav').css("width", "255px");
+  $('.container').css("opacity", "7");
+  setTimeout(function () {
+    menuStatus = "open";
+  }, 100)
+}
+
+// Close SideNav
+
+function closeNav() {
+  $('#sideNav').width(0);
+  menuStatus = "closed";
+}
+
+// Close Mobile Nav if user clicks on container.
+$('.container').click(() => {
+  if (menuStatus === "open")
+  closeNav();
+})
+
+$('.mobile__link').click( () => {
+  closeNav();
+  })
+
+  // Mobile Nav
+
+  $('#select-link').click(() =>
+  alert(hi))
